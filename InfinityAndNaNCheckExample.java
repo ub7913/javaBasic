2d3a946fe5c7ba3e90b1577bb95a1927778933e4
Park HOIL <ub7913@gmail.com>
2020-04-27 23:29:17 +0900

    add

--- InfinityAndNaNCheckExample.java
+++ InfinityAndNaNCheckExample.java
@@ -4,8 +4,8 @@ public class InfinityAndNaNCheckExample {
         int x = 5;
         double y = 0.0;
 
-        double z = x / y;
-        //double z = x % y;
+        //double z = x / y;
+        double z = x % y;
 
         System.out.println(Double.isInfinite(z));
         System.out.println(Double.isNaN(z));
