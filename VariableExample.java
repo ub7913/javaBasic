af80d66421bea8db4949e36839cf9fe9fcd9f03c
Park HOIL <ub7913@gmail.com>
2020-04-27 22:25:55 +0900

    add

---
+++ VariableExample.java
@@ -0,0 +1,15 @@
+
+public class VariableExample {
+    public static void main(String[] args){
+	   byte b1, b2;
+	   b1 = 30;
+	   b2 = 70;
+	   int result = b1 + b2 ;
+	   //System.out.println("30 + 70= " + result) ;
+	   byte b3 = 126;
+	   for (int i=0; i<100; i++){
+        b3++;
+        System.out.println(b3);
+       }
+	}	
+}	
